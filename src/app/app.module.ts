6c824e072b220d1b7e8fd25da090335e45d957e1
Graham Land <graham@allthingscloud.eu>
2019-03-04 09:49:03 +0000

    Added ability to read in environment variables at startup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HttpClientCallsService } from './http-client-calls.service';
 import { DisablecacheInterceptorService} from './disablecache-interceptor.service';
 
+import { EnvServiceProvider } from './env.service.provider';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -19,7 +21,7 @@ import { DisablecacheInterceptorService} from './disablecache-interceptor.servic
       provide: HTTP_INTERCEPTORS,
       useClass: DisablecacheInterceptorService ,
       multi: true
-  }],
+  }, EnvServiceProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
